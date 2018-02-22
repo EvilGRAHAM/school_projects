7f3a2413b54b3714cc386ad4969231a0bde37a67
Scott Graham <scott.grah95@gmail.com>
2018-02-21 18:58:40 -0700

    changed to dashboard

--- STAT_517/Project 03/Shiny/Kaisa Work on App/ui.R
+++ STAT_517/Project 03/Shiny/Kaisa Work on App/ui.R
@@ -1,57 +1,101 @@
 # Libraries ----------
 library(shiny, warn.conflicts = FALSE, quietly = TRUE)
 library(shinythemes, warn.conflicts = FALSE, quietly = TRUE)
+library(shinydashboard, warn.conflicts = FALSE, quietly = TRUE)
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
 
 
-# UI ----------
-fluidPage(
-  
-  # App Title ----------
-  titlePanel("Results of PANSS Testing")
-  
-  ,fluidRow(
-    
-    # Inputs ----------
-    column(
-      width = 4
-      ,offset = 0
-      ,wellPanel(
-        selectizeInput(
-          inputId = "question_set"
-          ,label = "Questions Displayed"
-          ,choices = 
-            c(
-              "Positive" = "P"
-              ,"Negative" = "N"
-              ,"Generic" = "G"
-            )
-        )
-        ,checkboxInput(
-          inputId = "by_lang"
-          ,"Facet by Language"
-          ,value = FALSE
-        )
-        ,numericInput(
-          inputId = "rater_num"
-          ,label = "Enter your Rater ID"
-          ,value = 1
-          ,min = 1
-          # ,max = 81
-          ,step = 1
-        )
-      )
-      ,verbatimTextOutput("logit")
-    )
+# Dashboard Header ----------
+db_header <- dashboardHeader(
+  title = "PANSS Testing"
+)
 
-    # Plots ---------
-    ,column(
-      width = 8
-      ,offset = 0
-      ,dataTableOutput("results")
-      ,plotOutput("hist")
-      ,plotOutput("violin")
-      ,plotOutput("prop")
+
+# Dashboard Sidebar ----------
+db_sidebar <- dashboardSidebar(
+  sidebarMenu(
+    selectizeInput(
+      inputId = "question_set"
+      ,label = "Questions Displayed"
+      ,choices = 
+        c(
+          "Positive" = "P"
+          ,"Negative" = "N"
+          ,"Generic" = "G"
+        )
+    )
+    ,checkboxInput(
+      inputId = "by_lang"
+      ,"Facet by Language"
+      ,value = FALSE
+    )
+    ,numericInput(
+      inputId = "rater_num"
+      ,label = "Enter your Rater ID"
+      ,value = 1
+      ,min = 1
+      # ,max = 81
+      ,step = 1
     )
   )
+)
+
+
+# Dashboard Body ----------
+db_body <- dashboardBody(
+  fluidRow(
+    column(
+      width = 12
+      ,box(
+        title = "Results"
+        ,width = NULL
+        ,dataTableOutput("results")
+      )
+    )
+  )
+  ,fluidRow(
+    column(
+      width = 6
+      ,box(
+        title = "Histograms"
+        ,width = NULL
+        ,plotOutput("hist")
+      )
+      ,box(
+        title = "Proportions"
+        ,width = NULL
+        ,plotOutput("prop")
+      )
+    )
+    ,column(
+      width = 6
+      ,box(
+        title = "Violin Plots"
+        ,width = NULL
+        ,plotOutput("violin")
+      )
+      ,box(
+        title = "Logistic Regression"
+        ,width = NULL
+        ,verbatimTextOutput("logit")
+      )
+    )
+  )
+)
+
+
+# UI ----------
+shinyUI(
+  function(request) {
+    dashboardPage(
+      skin = "black"
+      
+      ,db_header
+      
+      ,db_sidebar
+      
+      ,db_body
+      
+    )
+  }
 )
